e939aabf9ab96d3af8f292e82cb29b71e8429d50
loup <loup.quehec@gmail.com>
2025-08-07 15:32:23 +0200

    types

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
 import type { Database as ExtendedDatabase } from './type.db.extended';
+export * from './notifications';
+export * from './utils';
+export * from './type.db.extended';
 
 export type Database = ExtendedDatabase;
