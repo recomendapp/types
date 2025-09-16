33375e6718d526069c5f862eadc80e8c00c735c8
loup <loup.quehec@gmail.com>
2025-09-16 12:20:11 +0200

    replace user table by profile view

--- src/search/schemas/user.schema.ts
+++ src/search/schemas/user.schema.ts
@@ -9,7 +9,6 @@ export const userSearchQuerySchema = querySchema.extend({
 export const userSchema = z.object({
 	id: z.string(),
 	username: z.string(),
-	username_updated_at: z.string().nullable(),
 	full_name: z.string(),
 	bio: z.string().nullable(),
 	avatar_url: z.string().nullable(),
@@ -17,11 +16,10 @@ export const userSchema = z.object({
 	favorite_color: z.string().nullable(),
 	followers_count: z.number().min(0),
 	following_count: z.number().min(0),
-	created_at: z.string(),
 	background_url: z.string().nullable(),
 	premium: z.boolean(),
-	language: z.string(),
 	private: z.boolean(),
+	visible: z.boolean(),
 });
 
 export const userSearchResponseSchema = createPaginatedResponseSchema(userSchema);
